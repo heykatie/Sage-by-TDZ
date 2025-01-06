0bb30e404985cad6abd34c97a3fe4d9ef93e952c
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-01-06 13:31:03 -0500

    model and seed updates

--- app/models/organizer.py
+++ app/models/organizer.py
@@ -9,11 +9,11 @@ class Organizer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False, unique=True)
-    description = db.Column(db.String(1000), nullable=False)
-    logo = db.Column(db.String(255), nullable=False)
-    link = db.Column(db.String(255), nullable=False)
-    phone_number = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(2000), nullable=False)
+    logo = db.Column(db.String(1000), nullable=False)
+    link = db.Column(db.String(1000), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(1000), nullable=False)
 
     def to_dict(self):
         return {
