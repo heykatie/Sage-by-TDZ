a8fc137597397ab732fccca01a46f997a5571f7c
Katie Leong <155786253+heykatie@users.noreply.github.com>
2025-01-05 18:05:34 -0800

    connect delete modal to edit group page - Files: react-vite/src/components/GroupComponents/Group/Group.jsx react-vite/src/components/GroupComponents/GroupForm/GroupForm.jsx react-vite/src/components/GroupComponents/GroupModals/DeleteGroupModal/DeleteGroupModal.css react-vite/src/components/GroupComponents/GroupModals/DeleteGroupModal/DeleteGroupModal.jsx react-vite/src/router/index.jsx

--- react-vite/src/components/GroupComponents/GroupModals/DeleteGroupModal/DeleteGroupModal.jsx
+++ react-vite/src/components/GroupComponents/GroupModals/DeleteGroupModal/DeleteGroupModal.jsx
@@ -1,1 +1,22 @@
-export default function DeleteGroupModal() { return <div></div>} //{groupId, onConfirm, onClose}
+import './DeleteGroupModal.css';
+
+const DeleteGroupModal = ({ onConfirm, onCancel }) => {
+	return (
+		<div className='delete-modal'>
+			<div className='delete-modal-content'>
+				<p>Are you sure you want to delete this group?</p>
+				<div className='modal-buttons'>
+					<button className='confirm-delete' onClick={onConfirm}>
+						Yes, Delete
+					</button>
+					<button className='cancel-delete' onClick={onCancel}>
+						No, Go Back
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default DeleteGroupModal;
+
